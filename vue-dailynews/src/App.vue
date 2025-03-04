d1fd8a070ea9ecb5cce53fa3702251d89a7e14ea
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-03-04 14:29:45 +0100

    Création du projet + Nav.vue

---
+++ vue-dailynews/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Nav from "@/components/layout/Nav.vue";
+</script>
+
+<template>
+  <Nav/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-dailynews/src/components/layout/Nav.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Enregistrer un flux</RouterLink>
+    <RouterLink to="/">Liste des flux</RouterLink>
+    <RouterLink to="/">Préférences</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
