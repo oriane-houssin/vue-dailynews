4429900deb5ae39b55ba1e8b52afe4c1dd96caee
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2025-03-06 14:21:59 +0100

    ajout de fonction de suppression d'un flux depuis la liste des flux

--- vue-dailynews/src/views/FluxListView.vue
+++ vue-dailynews/src/views/FluxListView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-
 import {onMounted, ref} from "vue";
 import type {Flux} from "@/models/flux.ts";
 
-const fluxList = ref<Flux[]>([]);
+const fluxList = ref<Flux[]>([])
 
 //Charger la liste des flux
 const loadFluxList = () => {
@@ -11,9 +10,15 @@ const loadFluxList = () => {
   fluxList.value = savedFluxList;
 }
 
+//Supprimer un flux
+const deleteFlux = (index: number) => {
+  fluxList.value.splice(index, 1);
+  localStorage.setItem("fluxList", JSON.stringify(fluxList.value));
+}
+
 onMounted(() => {
   loadFluxList();
-});
+})
 </script>
 
 <template>
@@ -24,6 +29,7 @@ onMounted(() => {
       <a :href="item.url" target="_blank">
         <h4>{{item.title}}</h4>
       </a>
+      <button @click="deleteFlux(index)">Suprimer</button>
     </li>
   </ul>
 </div>
